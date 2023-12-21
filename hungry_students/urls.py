efe324e3b653f0ba6e73bcf9ffd4c9e14525c23d
JYOSNA PHILIP <142787944+jyosnaphilip@users.noreply.github.com>
2023-12-21 10:08:48 +0530

    set up base template

--- hungry_students/urls.py
+++ hungry_students/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Restaurants.views import index 
+from Restaurants.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index,name='index'),
+    path('Restaurants/',index,name='index'),
 ]
 
 
